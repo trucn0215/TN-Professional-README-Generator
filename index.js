e01803b407370f42eadfb41d5dd8f2e399df4990
Truc Nguyen <trucn0215@gmail.com>
2020-12-01 22:48:56 -0800

    add list of promted questions

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
+const inquirer = require ('inquirer');
+const fs = require('fs');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your Project title?",
+        name:"tittle"
+    },
+    {
+        type: "input",
+        message: "What is the description of your Project?",
+        name:"description"
+    },
+    {
+        type: "input",
+        message: "What is your Installation instruction?",
+        name:"installation"
+    },
+    {
+        type: "input",
+        message: "How do you use your project?",
+        name:"usage"
+    },
+    {
+        type: "input",
+        message: "What is the contribution of this project?",
+        name:"contribution"
+    },
+    {
+        type: "input",
+        message: "What is the User Story?",
+        name:"userStory"
+    },
+    {
+        type: "input",
+        message: "What is the acceptance criteria?",
+        name:"acceptanceCriteria"
+    },
+    {
+        type: "input",
+        message: "What is Test instruction? PROVIDE TEXT INSTRUCTION OR GIF OR LINK",
+        name:"test"
+    },
+    {
+        type: "list",
+        message: "What is license of the application? PICK ONE OF THE LICENSE BELOW!",
+        choises: 
+            [
+                "Apache License 2.0",
+                "BSD 3-Clause",
+                "BSD 2-Clause",
+                "GPL",
+                "LGPL",
+                "MIT license",
+                "Mozilla Public License 2.0"
+            ],
+        name:"license"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub Username?",
+        name:"github"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name:"email"
+    },
 ];
 
 // function to write README file
